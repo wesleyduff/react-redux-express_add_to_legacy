57a028bb927e2d836d9a38ba6353144f4ffe938d
Wesley <wesley.duff@charter.com>
2021-01-12 19:12:40 -0700

    Working : using npm module todo

--- routes/reactRoutes.js
+++ routes/reactRoutes.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var todo = require('todo');
+var util = require('util');
+console.log(`---- require works`)
+console.log(util.inspect(todo))
+
+var ejs = require('ejs')
+
+// router.get('/todo', function (req, res) {
+//     //res.sendFile(path.join(__dirname, '../client', 'build/todo.html'));
+//     res.render('../node_modules/todo/build/todo',{});
+// });
+
 router.get('/todo', function (req, res) {
     //res.sendFile(path.join(__dirname, '../client', 'build/todo.html'));
-    res.render('../node_modules/todo/build/todo',{});
+    res.render('pages/react-app/todo', {"app": todo.page, render:ejs.render});
 });
+
 
 
 module.exports = router;
