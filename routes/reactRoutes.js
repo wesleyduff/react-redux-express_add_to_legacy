8415ae3778d38261cec886d5fada1b6076d286b8
Wesley <wesley.duff@charter.com>
2021-01-12 18:18:57 -0700

    Using node_modules instead of referencing a client app module within the main application. This allows individual devs to contribute and to deploy individual apps instead of the whole applciation

--- routes/reactRoutes.js
+++ routes/reactRoutes.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path')
 
 router.get('/todo', function (req, res) {
     //res.sendFile(path.join(__dirname, '../client', 'build/todo.html'));
-    res.render('../client/build/todo', {});
+    res.render('../node_modules/todo/build/todo',{});
 });
+
 
 module.exports = router;
