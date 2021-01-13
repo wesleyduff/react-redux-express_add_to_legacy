ade640364e14b4a4f253c0989782f2a40f2d6da4
Wesley <wesley.duff@charter.com>
2021-01-12 20:13:11 -0700

    cleanup

--- routes/reactRoutes.js
+++ routes/reactRoutes.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const
+    express = require('express'),
+    router = express.Router(),
+    todo = require('todo'),
+    ejs = require('ejs')
+;
 
-var todo = require('todo');
-var util = require('util');
-console.log(`---- require works`)
-console.log(util.inspect(todo))
-
-var ejs = require('ejs')
-
+//OLD WAY
 // router.get('/todo', function (req, res) {
 //     //res.sendFile(path.join(__dirname, '../client', 'build/todo.html'));
 //     res.render('../node_modules/todo/build/todo',{});
 // });
 
+//BETTER WAY
 router.get('/todo', function (req, res) {
-    //res.sendFile(path.join(__dirname, '../client', 'build/todo.html'));
     res.render('pages/react-app/todo', {"app": todo.page, render:ejs.render});
 });
 
